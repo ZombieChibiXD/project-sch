a422e720618128388ce5359c378524fdf245b78d
Zombie Chibi XD <chibisd10@gmail.com>
2019-03-18 19:34:39 +0800

    Testing Vue with Laravel Passport

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,21 +10,17 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-      <div class="container">
-    <div class="jumbotron mt-5">
-            <h1 class="display-3">Website under building...</h1>
-            <img src="img/noimage.jpg">
-            <p class="lead">This is unfinished website I guess?</p>
-            <hr class="my-2">
-            <p>Still waiting for that guy to study Vue.JS, so now we're stuck on handling the backen'</p>
-            <p class="lead">
-                So Bayyy
-            </p>
-        </div>  
-      </div>
+    
+    <div id="app">
+      {{-- <passport-clients></passport-clients> --}}
+      <passport-authorized-clients></passport-authorized-clients>
+      {{-- <passport-personal-access-tokens></passport-personal-access-tokens> --}}
+
+    </div>
     
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{ mix('js/app.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
