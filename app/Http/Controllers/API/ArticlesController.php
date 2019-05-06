f4254163d8bec54e4aef864bed23853ab7dcdadd
ZombieChibiXD <chibisd10@gmail.com>
2019-05-06 08:37:39 +0800

    Added UserId system in ArticlesController@store

--- app/Http/Controllers/API/ArticlesController.php
+++ app/Http/Controllers/API/ArticlesController.php
@@ -7,9 +7,12 @@ use App\Http\Controllers\Controller;
 use App\Article;
 use App\Http\Resources\Article as ArticleResource;
 
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\User;
+
 class ArticlesController extends Controller
 {
-    private $pagination_limit = 10;
     /**
      * Display a listing of the resource.
      *
@@ -17,7 +20,7 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        $article = Article::orderBy('created_at','desc')->paginate($pagination_limit);
+        $article = Article::orderBy('created_at','desc')->paginate(10);
         return ArticleResource::collection($article);
     }
     public function tag($tag)
@@ -26,19 +29,19 @@ class ArticlesController extends Controller
         $article = "";
         switch($tag){
             case 'all':         
-                $article = Article::orderBy('created_at','desc')->paginate($pagination_limit);
+                $article = Article::orderBy('created_at','desc')->paginate(10);
                 break;
             case 'popular':     
-                $article = Article::orderBy('created_at','desc')->orderBy('views','desc')->paginate($pagination_limit);
+                $article = Article::orderBy('created_at','desc')->orderBy('views','desc')->paginate(10);
                 break;
             case 'sports':      
-                $article = Article::orderBy('created_at','desc')->where('tag','sports')->paginate($pagination_limit);
+                $article = Article::orderBy('created_at','desc')->where('tag','sports')->paginate(10);
                 break;
             case 'politics':    
-                $article = Article::orderBy('created_at','desc')->where('tag','politics')->paginate($pagination_limit);
+                $article = Article::orderBy('created_at','desc')->where('tag','politics')->paginate(10);
                 break;
             default:  
-                $article = Article::orderBy('created_at','desc')->where('tag',$tag)->paginate($pagination_limit);
+                $article = Article::orderBy('created_at','desc')->where('tag',$tag)->paginate(10);
 
         }
         return ArticleResource::collection($article);
@@ -60,51 +63,62 @@ class ArticlesController extends Controller
             'cover_image' => 'image|nullable|max:2999'
         ]);
         
-
-        // Handle File Upload
-        if($request->hasFile('cover_image')){
-            // Get filename with the extension
-            $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
-            // Get just filename
-            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-            // Get just ext
-            $extension = $request->file('cover_image')->getClientOriginalExtension();
-            // Filename to store
-            $fileNameToStore= $filename.'_'.time().'.'.$extension;
-            // Upload Image
-            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
-        } else {
-            if(!$isUpdate){     // If not update
-                $fileNameToStore = 'noimage.jpg';
+        $user= $request->user();
+        if($user->id>0){
+            // Handle File Upload
+            if($request->hasFile('cover_image')){
+                // Get filename with the extension
+                $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+                // Get just filename
+                $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+                // Get just ext
+                $extension = $request->file('cover_image')->getClientOriginalExtension();
+                // Filename to store
+                $fileNameToStore= $filename.'_'.time().'.'.$extension;
+                // Upload Image
+                $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+            } else {
+                if(!$isUpdate){     // If not update
+                    $fileNameToStore = 'noimage.jpg';
+                }
+            }
+            
+            $article = $isUpdate ? Article::findOrFail($request->input('article_id')) : new Article;
+            $article->id = $request->input('article_id');
+            $article->title = $request->input('title');
+            $article->tag = $request->input('tag');
+            $article->body = $request->input('body');
+            $article->views = $isUpdate ?$article->views:0;
+            $article->cover_image = $fileNameToStore;
+            
+            $oldId = $article->user_id;
+            //Modify this later
+            $article->user_id = $isUpdate ?$article->user_id:$user->id;
+            //$article->created_by = $request->isMethod('put')?$article->created_by:auth()->user()->id;
+            $HTTP_response_code = $isUpdate ? 200:201;
+            $HTTP_response_message = $isUpdate ?'Post has been Edited!':'Post has been Created!';
+            if(($isUpdate && $oldId!=$user->id)||!$isUpdate){
+                if($article->save())
+                return response()->json([
+                    'type' => 'success',
+                    'message' => $HTTP_response_message
+                ], $HTTP_response_code);
+                else {
+                    return response()->json([
+                        'type' => 'error',
+                        'message' => 'Action failed'
+                    ], 409);
+                }
+            }
+            else{
+                return response()->json([
+                    'message' => 'Unauthorized, belongs to other user',
+                    'status' => '401'
+                ], 401);
             }
         }
-
-        $article = $isUpdate ? Article::findOrFail($request->input('article_id')) : new Article;
-        $article->id = $request->input('article_id');
-        $article->title = $request->input('title');
-        $article->tag = $request->input('tag');
-        $article->body = $request->input('body');
-        $article->views = $isUpdate ?$article->views:0;
-        $article->cover_image = $fileNameToStore;
-        
-        //Modify this later
-        $article->user_id = $isUpdate ?$article->user_id:'0';
-        //$article->created_by = $request->isMethod('put')?$article->created_by:auth()->user()->id;
-        $HTTP_response_code = $isUpdate ? 200:201;
-        $HTTP_response_message = $isUpdate ?'Post has been Edited!':'Post has been Created!';
-        if($article->save())
-            return response()->json([
-                'type' => 'success',
-                'message' => $HTTP_response_message
-            ], $HTTP_response_code);
-        else {
-            return response()->json([
-                'type' => 'error',
-                'message' => 'Action failed'
-            ], 409);
-        }
     }
-
+    
     /**
      * Display the specified resource.
      *
