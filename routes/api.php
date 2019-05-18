f5b110b5008b9b3b1111a0fa5f14b6f82b011625
Zombie Chibi XD <43260238+ZombieChibiXD@users.noreply.github.com>
2019-05-18 14:01:42 +0800

    Update api.php
    Fixed lowercase

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,20 @@
 <?php
 use Illuminate\Http\Request;
 
-Route::get('articles', 'api\ArticlesController@index'); //show all
-Route::get('articles/tag/{tag}', ['uses' =>'api\ArticlesController@tag']); //show all base on tags
-Route::get('article/{id}', 'api\ArticlesController@show'); //show id only
+Route::get('articles', 'API\ArticlesController@index'); //show all
+Route::get('articles/tag/{tag}', ['uses' =>'API\ArticlesController@tag']); //show all base on tags
+Route::get('article/{id}', 'API\ArticlesController@show'); //show id only
 
 
 //Modifying articles middleware
 Route::group([
     'middleware' => 'auth:api'
   ], function() {
-      Route::get('article/{id}/edit', 'api\ArticlesController@edit'); //show id only w/out increment
-      Route::post('article', 'api\ArticlesController@store')->middleware('auth:api'); //create article
-      // Route::put('article', 'api\ArticlesController@store')->middleware('auth:api'); //edit article
-      Route::post('article', 'api\ArticlesController@store')->middleware('auth:api'); //edit article
-      Route::delete('article/{id}', 'api\ArticlesController@destroy')->middleware('auth:api'); //delete article
+      Route::get('article/{id}/edit', 'API\ArticlesController@edit'); //show id only w/out increment
+      Route::post('article', 'API\ArticlesController@store')->middleware('auth:api'); //create article
+      // Route::put('article', 'API\ArticlesController@store')->middleware('auth:api'); //edit article
+      Route::post('article', 'API\ArticlesController@store')->middleware('auth:api'); //edit article
+      Route::delete('article/{id}', 'API\ArticlesController@destroy')->middleware('auth:api'); //delete article
   });
 
 
@@ -22,11 +22,11 @@ Route::group([
   Route::group([
     'middleware' => 'auth:api'
   ], function() {
-    Route::post('article/{id}/comment', 'api\CommentsController@store')->middleware('auth:api'); //post comments
-    Route::get('article/{id}/comments', 'api\ArticlesController@comments')->middleware('auth:api'); //post comments
-    Route::post('article/comment/edit/{id}', 'api\CommentsController@update')->middleware('auth:api'); //edit comments
-    Route::delete('article/comment/delete/{id}', 'api\CommentsController@destroy')->middleware('auth:api'); //DELETE comments
-    //Route::get('article/comment/show/all/debug', 'api\CommentsController@index')->middleware('auth:api'); //Testing purposes
+    Route::post('article/{id}/comment', 'API\CommentsController@store')->middleware('auth:api'); //post comments
+    Route::get('article/{id}/comments', 'API\ArticlesController@comments')->middleware('auth:api'); //post comments
+    Route::post('article/comment/edit/{id}', 'API\CommentsController@update')->middleware('auth:api'); //edit comments
+    Route::delete('article/comment/delete/{id}', 'API\CommentsController@destroy')->middleware('auth:api'); //DELETE comments
+    //Route::get('article/comment/show/all/debug', 'API\CommentsController@index')->middleware('auth:api'); //Testing purposes
   });
 
 
